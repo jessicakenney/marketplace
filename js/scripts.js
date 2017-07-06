5f5df2631cc005f91555b858949b7709b032368f
Jessica Sheridan and Shyamal Punekar <shyamal@>
2017-07-06 11:06:30 -0700

    Additional input selection with select form and radiobuttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
   $("#userInfoForm").submit(function(event) {
-    alert("hello");
+
     var fullName = $("input#fullName").val();
     var email = $("input#email").val();
     var homeAddress = $("input#homeAddress").val();
+    var jewelry = $("#jewelry").val();
+    var quantity = $("input:radio[name=quantity]:checked").val();
 
     $(".fullName").text(fullName);
+    $(".jewelry").text(jewelry);
+    $(".quantity").text(quantity);
+
     $("#receipt").show();
     event.preventDefault();
   });
